989880090a8fcf87287c6f853f714e979bf2e30b
rafaeldellaquila <rafadellaquila@gmail.com>
2021-06-20 17:58:51 -0300

    teste hhusky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 const foo = 'blabla'
 const foo = 'blabla'
 const foo = 'blabla'
+const foo = 'blabla'
+const foo = 'blabla'
 
 export default function Home() {
   return <h1>Boilerplate do Rafinha test</h1>
